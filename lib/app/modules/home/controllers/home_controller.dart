d16f419309cb33324d2c54eadf721879fb4969ad
Hamid <montazeria90@gmail.com>
2021-06-22 22:28:08 +0430

    Fix home

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,7 +1,30 @@
+import 'package:alo_self/app/api/restaurant/restaurant_api.dart';
+import 'package:alo_self/app/model/restaurant.dart';
+import 'package:alo_self/app/modules/login/controllers/login_controller.dart';
+import 'package:alo_self/app/modules/login/views/login_view.dart';
+import 'package:alo_self/app/utils/custom_pair.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-class HomeController extends GetxController {  
-  var navBarIndex = 0.obs;
+class HomeController extends GetxController {
+  final nameController = TextEditingController().obs;
+  final areaController = TextEditingController().obs;
+  final addressController = TextEditingController().obs;
+  final serviceAreasController = TextEditingController().obs;
+  final workHourController = TextEditingController().obs;
+  final deliverCostController = TextEditingController().obs;
+  List<CustomPair<String, Rx<TextEditingController>>> get addRestaurantControllers => [
+        CustomPair('Name', nameController),
+        CustomPair('Area', areaController),
+        CustomPair('Address', addressController),
+        CustomPair('Supporting Areas', serviceAreasController),
+        CustomPair('Hours', workHourController),
+        CustomPair('Deliver Cost', deliverCostController),
+      ];
+
+  final restaurantApi = RestaurantApi();
+
   @override
   void onInit() {
     super.onInit();
@@ -15,7 +38,23 @@ class HomeController extends GetxController {
   @override
   void onClose() {}
 
-  void updateIndex(int index) {
-    navBarIndex.value = index;
+  Future<void> logout() async {
+    await GetStorage().remove('token');
+    Get.lazyPut<LoginController>(() => LoginController());
+    await Get.offAll(() => LoginView());
+    update();
+  }
+
+  Future<Restaurant?> addRestaurant() async {
+    final _res = await restaurantApi.addRestaurant(
+      name: nameController.value.text,
+      area: areaController.value.text,
+      address: addressController.value.text,
+      service_areas: [serviceAreasController.value.text],
+      work_hour: workHourController.value.text,
+      deliver_cost: int.parse(deliverCostController.value.text),
+    );
+
+    return _res;
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,155 +1,167 @@
 import 'package:alo_self/app/common_widgets/custom_app_bar.dart';
+import 'package:alo_self/app/common_widgets/custom_form.dart';
+import 'package:alo_self/app/common_widgets/custom_snackbar.dart';
 import 'package:alo_self/generated/locales.g.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => Scaffold(
-        appBar: CustomAppBar(
-          title: LocaleKeys.pageTitles_home.tr,
-        ),
-        body: Container(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(25).copyWith(bottom: 0),
-                  child: SizedBox(
-                    height: 200,
-                    child: Column(
-                      children: [
-                        Text(
-                          LocaleKeys.home_title.tr,
-                          style: Get.textTheme.headline1,
-                        ),
-                        SizedBox(height: 35),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Text(
-                              LocaleKeys.home_totalRequests.tr + ": 2",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17,
-                              ),
-                            ),
-                            Container(
-                              height: 25,
-                              width: 2,
-                              color: Colors.grey,
-                            ),
-                            Text(
-                              LocaleKeys.home_totalDeliveries.tr + ": 1",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Text(
-                          LocaleKeys.home_cash.tr + ": 20,000 تومان",
-                          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22, color: Colors.green[800]),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 45),
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFC8E6C9),
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(35),
-                        topRight: Radius.circular(35),
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: 'Parham Food',
+        onTap: () async {
+          await controller.logout();
+        },
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(25).copyWith(bottom: 0),
+                child: SizedBox(
+                  height: 200,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Parham, the Best',
+                        style: Get.textTheme.headline1,
                       ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            _buildIconView(Icons.delivery_dining, "قبول سفارش"),
-                            _buildIconView(Icons.add, "ثبت سفارش"),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            _buildIconView(Icons.history, "سوابق ارسال"),
-                            _buildIconView(Icons.support_agent, "ارتباط با پشتیبانی"),
-                          ],
-                        ),
-                      ],
-                    ),
+                      SizedBox(height: 35),
+                      // Row(
+                      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      //   children: [
+                      //     Text(
+                      //       LocaleKeys.home_totalRequests.tr + ": 2",
+                      //       style: TextStyle(
+                      //         fontWeight: FontWeight.bold,
+                      //         fontSize: 17,
+                      //       ),
+                      //     ),
+                      //     Container(
+                      //       height: 25,
+                      //       width: 2,
+                      //       color: Colors.grey,
+                      //     ),
+                      //     Text(
+                      //       LocaleKeys.home_totalDeliveries.tr + ": 1",
+                      //       style: TextStyle(
+                      //         fontWeight: FontWeight.bold,
+                      //         fontSize: 17,
+                      //       ),
+                      //     ),
+                      //   ],
+                      // ),
+                      // SizedBox(
+                      //   height: 30,
+                      // ),
+                      // Text(
+                      //   LocaleKeys.home_cash.tr + ": 20,000 تومان",
+                      //   style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22, color: Colors.green[800]),
+                      // ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 45),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFC8E6C9),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35),
+                      topRight: Radius.circular(35),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _buildIconView(Icons.restaurant, "Restaurant List"),
+                          _buildIconView(
+                            Icons.add,
+                            "Add Restaurant",
+                            onTap: () => CustomForm.show(
+                              context,
+                              formTitle: 'Add Restaurant',
+                              textFields: controller.addRestaurantControllers
+                                  .map((e) => TextField(
+                                        controller: e.second.value,
+                                        decoration: InputDecoration(
+                                          labelText: e.first,
+                                        ),
+                                      ))
+                                  .toList(),
+                              buttonOnPressed: () async {
+                                final _res = await controller.addRestaurant();
+                                if (_res != null) {
+                                  Get.back();
+                                  CustomSnackBar.show(
+                                    'Succeed',
+                                    'Restaurant ${_res.name} added',
+                                  );
+
+                                }
+                              },
+                              buttonText: 'Add',
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _buildIconView(Icons.history, 'Send History'),
+                          _buildIconView(Icons.support_agent, 'Contact Us'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
-        bottomNavigationBar: SalomonBottomBar(
-          margin: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-          itemPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          currentIndex: controller.navBarIndex.value,
-          onTap: (index) => controller.updateIndex(index),
-          items: [
-            SalomonBottomBarItem(
-              icon: Icon(Icons.home),
-              title: Text(LocaleKeys.pageTitles_home.tr),
-            ),
-            SalomonBottomBarItem(
-              icon: Icon(Icons.request_page),
-              title: Text(LocaleKeys.pageTitles_request.tr),
-            ),
-            SalomonBottomBarItem(
-              icon: Icon(Icons.send_rounded),
-              title: Text(LocaleKeys.pageTitles_delivery.tr),
-            ),
-            SalomonBottomBarItem(
-              icon: Icon(Icons.person),
-              title: Text(LocaleKeys.pageTitles_profile.tr),
-            ),
-          ],
         ),
       ),
     );
   }
 
-  Widget _buildIconView(IconData iconData, String text) {
+  Widget _buildIconView(IconData iconData, String text, {VoidCallback? onTap}) {
     return Flexible(
-      child: Column(
-        children: [
-          Icon(
-            iconData,
-            color: Colors.green[600],
-            size: 60,
-          ),
-          SizedBox(height: 10),
-          Text(
-            text,
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 20,
+      child: InkWell(
+        onTap: () {
+          if (onTap != null) {
+            onTap();
+          }
+        },
+        child: Column(
+          children: [
+            Icon(
+              iconData,
+              color: Colors.green[600],
+              size: 60,
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
