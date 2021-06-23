a61311fc690891074f26a2b2efef343fa41efff1
Hamid <montazeria90@gmail.com>
2021-06-23 13:33:55 +0430

    add user models

---
+++ lib/app/model/user_profile.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_profile.g.dart';
+
+@JsonSerializable()
+class UserProfile {
+  final String first_name;
+  final String last_name;
+  final String area;
+  final String address;
+  final int? credit;
+  final List? orders_history;
+  final List? favorits;
+  const UserProfile({
+    required this.first_name,
+    required this.last_name,
+    required this.area,
+    required this.address,
+    this.credit,
+    this.orders_history,
+    this.favorits,
+  });
+  factory UserProfile.fromJson(Map<String, dynamic> json) => _$UserProfileFromJson(json);
+  Map<String, dynamic> toJson() => _$UserProfileToJson(this);
+}
